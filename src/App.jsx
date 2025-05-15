fbedcb21b31be097ce85eb7842fe3e507ee9f92f
Rohit Ware <rohitware99@gmail.com>
2025-05-16 01:12:30 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Comment from './useState/Comment'
+import Loading from './useState/Loading'
+
+function App() {
+
+  return (
+    <>
+   <h2>useState Hook in react</h2>
+   <Comment />
+   <Loading />
+    </>
+  )
+}
+
+export default App
