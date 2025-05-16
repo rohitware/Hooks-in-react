0eb597cef512fa23a1cf47e2a274379c7735bb6f
Rohit Ware <rohitware99@gmail.com>
2025-05-17 01:10:09 +0530

    Shift Browser router from app.jsx to main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
 import UseStateRoutes from "./useState/UseStateRoutes";
 import UseEffectRoutes from "./useEffect/UseEffectRoutes";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* Layout wraps all these routes */}
-        <Route path="/" element={<Layout />}>
-          <Route path="use-state" element={<UseStateRoutes />} />
-          <Route path="use-effect" element={<UseEffectRoutes />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      {/* Layout wraps all these routes */}
+      <Route path="/" element={<Layout />}>
+        <Route path="use-state" element={<UseStateRoutes />} />
+        <Route path="use-effect" element={<UseEffectRoutes />} />
+      </Route>
+    </Routes>
   );
 }
 
