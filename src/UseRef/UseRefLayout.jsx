9342714a466d56536970fb7efb5de026fd276c41
Rohit Ware <rohitware99@gmail.com>
2025-05-30 02:41:36 +0530

    Rendering components in layout

--- src/UseRef/UseRefLayout.jsx
+++ src/UseRef/UseRefLayout.jsx
@@ -1,9 +1,19 @@
+import Conuter2UseRef from "./Counter2UseRef";
 import CounterUseRef from "./CounterUseRef";
+import AccessDomUseRef from "./AccessDomUseRef";
 
 function UseRefLayout() {
   return (
     <>
       <CounterUseRef />
+      <br></br>
+      <br></br>
+
+      <Conuter2UseRef />
+      <br />
+      <br />
+
+      <AccessDomUseRef />
     </>
   );
 }
